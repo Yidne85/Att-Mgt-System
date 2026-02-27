e954334c7c079b4674299b8a64b8ffc4ff60c96f
Yidnekachew Tafese <107847098+Yidne85@users.noreply.github.com>
2026-02-27 15:57:31 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: { extend: {} },
+  theme: { extend: {colors: {
+  brand: {
+    DEFAULT: "#A61C1C",
+    dark: "#8E1515",
+    light: "#C73434",
+  },
+},} },
   plugins: [],
 } satisfies Config;
