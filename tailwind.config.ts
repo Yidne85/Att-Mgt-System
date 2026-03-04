e1f9ef66ae0f71b626abd5dc6ac0ca8f09601aa5
Yidnekachew Tafese <107847098+Yidne85@users.noreply.github.com>
2026-03-04 13:04:37 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,11 @@
 import type { Config } from "tailwindcss";
 export default {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: [
+  "./app/**/*.{ts,tsx}",
+  "./components/**/*.{ts,tsx}",
+  "./pages/**/*.{ts,tsx}",
+  "./lib/**/*.{ts,tsx}",
+],
   theme: { extend: {} },
   plugins: [],
 } satisfies Config;
