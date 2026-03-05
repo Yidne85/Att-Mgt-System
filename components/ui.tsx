b68a2052af1b636ce55f7a16ee828633898d77f8
Yidnekachew Tafese <107847098+Yidne85@users.noreply.github.com>
2026-03-05 11:07:58 +0300

    Update ui.tsx

--- components/ui.tsx
+++ components/ui.tsx
@@ -61,10 +61,10 @@ export function Button({
 
   const styles =
     variant === "primary"
-      ? "bg-brand text-white hover:bg-brand-dark shadow-sm"
+      ? "bg-brand text-slate-800 hover:bg-brand-dark shadow-sm"
       : variant === "secondary"
       ? "bg-slate-100 text-slate-900 hover:bg-slate-200"
-      : "bg-red-600 text-white hover:bg-red-700";
+      : "bg-red-600 text-slate-800 hover:bg-red-700";
 
   return <button {...props} className={`${base} ${styles} ${className}`} />;
 }
